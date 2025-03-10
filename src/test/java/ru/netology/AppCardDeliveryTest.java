3eb8fe450f76e82b63dc04832baf945bc816f554
Artemiy Mazur <artemiymazur@yandex.ru>
2025-03-10 12:16:35 +0300

    Second Commit

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -3,40 +3,51 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.Keys;
 
+import java.text.MessageFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.openqa.selenium.Keys.BACK_SPACE;
+import static org.openqa.selenium.Keys.CONTROL;
 
 public class AppCardDeliveryTest {
 
     @Test
     void shouldSendForm(){
         Selenide.open("http://localhost:9999");
-        $$("form").last().click();
         $("[type='text']").setValue("Краснодар");
-        LocalDate date = LocalDate.now();
-        date = date.plusDays(2);
-        $("[type='tel']").setValue(date.toString());
+
+        LocalDate meetingDate = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateForInput = meetingDate.format(formatter);
+        SelenideElement dateInput = $("[data-test-id='date'] input.input__control");
+        dateInput.doubleClick();
+        dateInput.sendKeys(BACK_SPACE);
+        dateInput.setValue(dateForInput);
+        dateInput.shouldHave(Condition.value(dateForInput));
+
         $("[name='name']").setValue("Мазур Артемий");
+
         $("[name='phone']").setValue("+78005553535");
+
         $("label[data-test-id='agreement']").click();
-        $("[type='button").click();
-        $(Selectors.withText("Успешно")).should(Condition.visible, Duration.ofSeconds(15));
+
+        $(Selectors.withText("Забронировать")).click();
+
+        $(Selectors.withText("Успешно!"))
+                .shouldHave(exactText("Успешно!"), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
 
     }
 
